4d233bbaae304c59cac9ca989c27ecb3988304ab
Henish Patel <53644709+henishpatel@users.noreply.github.com>
2021-02-08 14:34:59 +0530

    Add files via upload

---
+++ A_Way_Too_Long_Words.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define pb push_back 
+#define int long long int 
+typedef unsigned long long ull;
+#define mod 1000000007
+#define xx first
+#define yy second
+#define codec ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);;
+typedef vector<int> vi;
+#define deb(x) cout<<#x<<" "<<x<<endl;
+#define all(t) (t).begin(),(t).end()
+typedef pair<int, int> pi;
+#define pie 3.1415926535
+#define output(x) cout << ( x ? "YES" : "NO" ) << '\n' ;
+void solve(){
+    int n,length;
+    string str;
+    cin >> n;
+    rep(i,0,n){
+        cin >> str;
+        length = str.length();
+        if(length > 10){
+            cout << str[0] << length-2 << str[length-1] << endl;
+        }else{
+            cout << str << endl;
+        }
+    }
+}
+
+signed main(){
+    int T=1;
+    //cin >> T;
+    while(T--){
+        solve();
+    }
+}
+
